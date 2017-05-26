7c705b4c64c7b1b6af8c5e35726e16869b734265
Hannah Park <teyang610@gmail.com>
2017-05-26 12:16:35 -0700

    MVC_class

--- MVCDemo/src/mvcdemo/CalculatorController.java
+++ MVCDemo/src/mvcdemo/CalculatorController.java
@@ -5,10 +5,36 @@
  */
 package mvcdemo;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author hyunyoungpark
  */
 public class CalculatorController {
+    private CalculatorModel theModel;
+    private CalculatorView theView;
     
-}
+    public CalculatorController(CalculatorView theView, CalculatorModel theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+        this.theView.addCalculateListener(new calculateListner());
+      
+    }
+        
+        class calculateListner implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int no1, no2;
+                no1 = theView.getFirstNumber();
+                no2 = theView.getSecondNumber();
+                theModel.addTwoNumbers(no1, no2);
+                int result = theModel.getCalcvalue();
+                theView.setCalcSolution(result);
+                
+            }
+            
+        }
+    }
--- MVCDemo/src/mvcdemo/CalculatorModel.java
+++ MVCDemo/src/mvcdemo/CalculatorModel.java
@@ -10,5 +10,14 @@ package mvcdemo;
  * @author hyunyoungpark
  */
 public class CalculatorModel {
+    private int CalValue;
+    public void addTwoNumbers(int no1, int no2)
+    {
+        CalValue = no1 + no2;
+    }
+    
+    public int getCalcvalue(){
+        return CalValue;
+    }
     
 }
