da62e58a381eb056f8deb6018f0476ef741a8efc
Hannah Park <teyang610@gmail.com>
2017-06-12 11:08:08 -0700

    Class

---
+++ SortingTest/src/SortingDemoTest/Sortdemo1.java
@@ -0,0 +1,13 @@
+
+package SortingDemoTest;
+
+
+public class Sortdemo1 {
+
+    public static void main(String[] args) {
+        int[] num = new int[10];
+        for (int i = 0; i < 10; i++) {
+            num[i] = (int) (Math.random()*10);
+        }       
+    }
+} 
