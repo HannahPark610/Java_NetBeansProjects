49211c406ea702f6ba41c0a2a367ba8ef1dc5436
Hannah Park <teyang610@gmail.com>
2017-06-15 13:29:05 -0700

    Update

--- FinalProjectGUI/src/FinalGUI/DataBase.java
+++ FinalProjectGUI/src/FinalGUI/DataBase.java
@@ -8,6 +8,7 @@ package FinalGUI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -22,18 +23,29 @@ public class DataBase {
         Connection myconObj = null;
         Statement mystatObj = null;
         ResultSet myresObj = null;
+        ResultSetMetaData mymeta = null;
         String query = "Select * from Hannah.ShoesStockTable";
 
         try {
             myconObj = DriverManager.getConnection("jdbc:derby://localhost:1527/StockDatabase", "Hannah", "12345");
             mystatObj = myconObj.createStatement();
             myresObj = mystatObj.executeQuery(query);
+            mymeta = myresObj.getMetaData();
+            int coloumnNo = mymeta.getColumnCount();
+//            System.out.println(coloumnNo);
+            for (int i = 1; i<=coloumnNo; i++){
+                System.out.println(mymeta.getColumnName(i)+"\t");
+            }
             while (myresObj.next()) {
-                String typeOfShoes = myresObj.getString("TypeOfShoes");
-                String color = myresObj.getString("Color");
-                Double size = myresObj.getDouble("Size");
-                int stock = myresObj.getInt("Stock");
-                System.out.println(typeOfShoes + "\t  " + color + "\t  " + size + "\t  "+ stock);
+                for (int i = 1; i<=coloumnNo; i++){
+                     System.out.println(myresObj.getObject(i)+"\t");
+                }
+                System.out.println();
+//                String typeOfShoes = myresObj.getString("TypeOfShoes");
+//                String color = myresObj.getString("Color");
+//                Double size = myresObj.getDouble("Size");
+//                int stock = myresObj.getInt("Stock");
+//                System.out.println(typeOfShoes + "\t  " + color + "\t  " + size + "\t  "+ stock);
             }
         } catch (SQLException e) {
             e.printStackTrace();
